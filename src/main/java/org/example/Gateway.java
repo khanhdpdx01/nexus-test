5985a400a54c8ae68359b93d3f655f8e698488a1
khanhdpdx01 <tankhanh2k1@gmail.com>
2022-07-28 17:24:55 +0700

    Frist commit

---
+++ src/main/java/org/example/Gateway.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Gateway {
+    public int plus(int a, int b) {
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
+}
